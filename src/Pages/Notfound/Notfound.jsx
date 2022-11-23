0f22bbebd3cbefe3761238dd610a57c46701081d
Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2022-11-24 01:04:39 +0600

    routes added

---
+++ src/Pages/Notfound/Notfound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS25j6b20Qg0OBpPf7MPJ1b_m_ffNu-aakBk0y0xsUlBvZXLV-E8TkXAr_hSr9m-W_WOAk&usqp=CAU"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Notfound;
