0546ee160e09e1b0a5baab0862cd3dc40729860d
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2022-11-28 15:14:13 +0600

    users will be redirect to login if condition not fulfilled

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,27 +1,17 @@
 import React, { useContext } from "react";
-import { CirclesWithBar } from "react-loader-spinner";
+import { Navigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthProvider/AuthProvider";
 import useAdmin from "../Hooks/useAdmin";
 
 const AdminRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, logOutUser } = useContext(AuthContext);
   const [admin, loading] = useAdmin(user?.email);
   if (loading) {
     return (
-      <div className="flex justify-center items-center">
-        <CirclesWithBar
-          height="100"
-          width="100"
-          color="#4fa94d"
-          wrapperStyle={{}}
-          wrapperclassName=""
-          visible={true}
-          outerCircleColor=""
-          innerCircleColor=""
-          barColor=""
-          ariaLabel="circles-with-bar-loading"
-        />
-      </div>
+      (<Navigate to="/login"></Navigate>),
+      logOutUser()
+        .then(() => {})
+        .catch(() => {})
     );
   }
   if (user && admin) {
--- src/Routes/SellerRoute.jsx
+++ src/Routes/SellerRoute.jsx
@@ -1,27 +1,17 @@
 import React, { useContext } from "react";
-import { CirclesWithBar } from "react-loader-spinner";
+import { Navigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthProvider/AuthProvider";
 import useSeller from "../Hooks/useSeller";
 
 const SellerRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, logOutUser } = useContext(AuthContext);
   const [isSeller, loading] = useSeller(user?.email);
   if (loading) {
     return (
-      <div className="flex justify-center items-center">
-        <CirclesWithBar
-          height="100"
-          width="100"
-          color="#4fa94d"
-          wrapperStyle={{}}
-          wrapperclassName=""
-          visible={true}
-          outerCircleColor=""
-          innerCircleColor=""
-          barColor=""
-          ariaLabel="circles-with-bar-loading"
-        />
-      </div>
+      (<Navigate to="/login"></Navigate>),
+      logOutUser()
+        .then(() => {})
+        .catch(() => {})
     );
   }
   if (user && isSeller) {
