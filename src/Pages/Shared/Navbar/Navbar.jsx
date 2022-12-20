3fd36b68407d8c28d6d87de66efb904678310fb8
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2022-12-21 01:01:56 +0600

    navbar redesigned

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -15,21 +15,28 @@ const Navbar = () => {
 
   const menuItems = (
     <>
-      <li className="hover:bg-primary text-white font-bold">
+      <li className="hover:bg-primary text-black hover:text-white font-bold">
         <Link to="/">Home</Link>
       </li>
-      <li className="hover:bg-primary text-white font-bold">
+      <li className="hover:bg-primary text-black hover:text-white font-bold">
         <Link to="/blogs">Blogs</Link>
       </li>
-      <li className="hover:bg-primary text-white font-bold">
+      <li className="hover:bg-primary text-black hover:text-white font-bold">
         <Link to="/dashboard">Dashboard</Link>
       </li>
       {user?.email ? (
-        <li className="hover:bg-primary text-white font-bold">
-          <button onClick={logOut}>Logout</button>
+        <li>
+          <button onClick={logOut}>
+            <img
+              className="img-fluid"
+              style={{ width: "28px" }}
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/OOjs_UI_icon_logOut-ltr.svg/2048px-OOjs_UI_icon_logOut-ltr.svg.png"
+              alt=""
+            />
+          </button>
         </li>
       ) : (
-        <li className="hover:bg-primary text-white font-bold">
+        <li className="hover:bg-primary text-black hover:text-white font-bold">
           <Link to="/login">Login</Link>
         </li>
       )}
@@ -37,14 +44,14 @@ const Navbar = () => {
   );
 
   return (
-    <div className=" bg-primary mb-6">
+    <div className="mb-6 shadow-sm">
       <div className="navbar max-w-[1440px] mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <label tabIndex={0} className="cursor-pointer lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-white"
+                className="h-5 w-5 text-black"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -59,13 +66,20 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary rounded-box w-52"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-white rounded-box w-52"
             >
               {menuItems}
             </ul>
           </div>
           <Link to="/" className=" text-xl font-bold text-white">
-            Resell-BD
+            <div className="flex items-center">
+              <img
+                className="w-12"
+                src="https://i.ibb.co/TkjGrHR/png-transparent-computer-icons-retail-reseller-others-miscellaneous-retail-omnichannel-thumbnail-rem.png"
+                alt=""
+              />
+              <p className="text-black">Resell-BD</p>
+            </div>
           </Link>
         </div>
 
